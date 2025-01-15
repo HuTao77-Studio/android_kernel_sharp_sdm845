531cded861d51aeb7030ac5a76c34dbf999dc1cd
admin <paper@localhost>
2025-01-15 18:20:09 +0800

    [DEBUG] qpnp-power-on: Press the pwr key to force a kernel panic * If you get stuck at the boot logo during kernel debugging,   you can trigger a kernel panic by pressing the pwr key,   which will trigger a Sharp panic log dump
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -1013,6 +1013,11 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 		input_sync(pon->pon_input);
 	}
 
+#ifdef CONFIG_SHARP_KERNEL_PANIC_ON_PWRKEY
+	if (cfg->pon_type == PON_KPDPWR && key_status)
+		panic("Trigger by pwr key");
+#endif
+
 	input_report_key(pon->pon_input, cfg->key_code, key_status);
 	input_sync(pon->pon_input);
 
