5f5584fa246924ecfe1bad93fff8c62f6396b8a7
Fuutao <fuutaoao@gmail.com>
2025-01-18 13:02:00 +0800

    [DEBUG] drivers: power: reset: Add kernel panic reboot to recovery support
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -390,10 +390,20 @@ static void msm_restart_prepare(const char *cmd)
 		pr_info("Forcing a warm reset of the system\n");
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
+#ifdef CONFIG_PANIC_REBOOT_RECOVERY
+	if (true)
+#else
 	if (force_warm_reboot || need_warm_reset)
+#endif
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
+#ifdef CONFIG_PANIC_REBOOT_RECOVERY
+		qpnp_pon_set_restart_reason(PON_RESTART_REASON_RECOVERY);
+			__raw_writel(0x77665502, restart_reason);
+		flush_cache_all();
+		return;
+#endif
 
 	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
